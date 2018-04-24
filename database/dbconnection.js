7c2e37e6ddd592f92699cced558f55568dc1b014
Ranjan <buildoncecorp@gmail.com>
2018-04-24 02:19:06 -0400

    'changes'

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -3,10 +3,10 @@ var mysql=require('mysql');
 
  var connection=mysql.createConnection({
  
-host:'lmag6s0zwmcswp5w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
- user:'hq7pb5pfmwesjhu3',
- password:'h7uyg1ah9o4yhnzx',
- database:'d905s42lv3a82zqu'
+host:'**',
+ user:'**',
+ password:'**',
+ database:'**'
  
 });
 
